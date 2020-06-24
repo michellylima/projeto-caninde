bc033242eb431e34a2abab5cf3a171d5cf6d9a81
Clinton <clinton_ferraz@hotmail.com>
2020-06-24 18:25:18 -0300

    avançando na construção da página

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './footer.css';
+
 import logoFooter from '../../assets/logo-footer.png';
+import facebook from '../../assets/facebook.svg'
+import instagram from '../../assets/instagram.svg'
+import linkedin from '../../assets/linkedin.svg'
+import behance from '../../assets/behance.svg'
+
+
 
 export default function Footer(){
     return(
         <div className='footer-container'>
-            <a href='/#'><img id='header-menu-logo' alt='' src={logoFooter}></img></a>
+            <a href='/#'><img id='footer-logo' alt='' src={logoFooter}></img></a>
             <div>
                 <p>
                     Empresa Junior de Design da UFG<br/>
@@ -20,19 +27,26 @@ export default function Footer(){
 
 
                 </p>
-
             </div>
+
+            <div>
                 <p>
                     Qualquer dúvida, contate nosso:<br/>
-            
-            <div className='whatsappDiv'><a href='/#' >Whatsapp</a></div>
-               
+                </p>
+                <div className='whatsappDiv'><a href='/#' >Whatsapp</a></div>
+                <p>
                     Conheça mais a gente:<br/>
                 </p>
-            <div>
-
-
+                <a href='https://www.facebook.com/canindesign/' target="_blank" rel='noopener noreferrer'><img className='socialmedia facebook' src={facebook} alt='' /></a>
+                <a href='https://www.instagram.com/canindesign/' target="_blank" rel='noopener noreferrer'><img className='socialmedia instagram' src={instagram} alt='' /></a>
+                <a href='https://www.linkedin.com/company/canindesign/about/' target="_blank" rel='noopener noreferrer'><img className='socialmedia linkedin' src={linkedin} alt='' /></a>
+                <a href='https://www.behance.net/canindesign' target="_blank" rel='noopener noreferrer'><img className='socialmedia behance' src={behance} alt='' /></a>
+                 
+            
+                
             </div>
+
+
 
         </div>
     );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,15 +3,36 @@ import React from 'react';
 
 import './styles.css';
 import HeaderMenu from '../../components/header-menu/header-menu'
+import Imagem from '../../assets/imagem.jpg'
 import Footer from '../../components/footer/footer';
 
 export default function Home(){
     return(
-        <>
-        <HeaderMenu />
-        <div style={{height: '30rem'}}></div>
-        <Footer />
-        </>
+        <div className='home-container'>
+            <HeaderMenu />
+                <img id='banner-principal' src={Imagem} alt=''/>
+
+                <section className='secao1'>
+                    <div className='secao1-container'>
+                        <div className='secao1-esquerda' >
+                                <h1><span style={{color: '#0BFEFF'}}>Design</span> para a <br/>sua necessidade <br/>de voar cada vez<br/> mais alto_</h1>
+                        </div>
+
+                        <div className='secao1-direita-flutante'>
+
+                        </div>
+                    </div>
+                </section>
+
+                <section className='secao2'>
+                    <div className='secao2-container'>
+                        <h1>Trabalhamos com sonhos <br/>e mais que isso, tornamos <br/>eles <span style={{color: '#0BFEFF'}}>realidade</span>_ </h1>
+                    </div>
+                </section>
+
+                <div style={{height: '30rem'}}></div>
+            <Footer />
+        </div>
     );
 
 
