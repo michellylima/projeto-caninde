e5a8645c4c4cb7a700bdda1910bbdbc215bb433a
Clinton <clinton_ferraz@hotmail.com>
2020-06-25 16:59:00 -0300

    início da criação da seção de projetos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 
 import './styles.css';
-import HeaderMenu from '../../components/header-menu/header-menu'
-import Imagem from '../../assets/imagem.jpg'
+import HeaderMenu from '../../components/header-menu/header-menu';
 import Footer from '../../components/footer/footer';
+import Imagem from '../../assets/imagem.jpg';
+import ImagemProjeto from '../../assets/imagem_projeto.jpg';
 
 export default function Home(){
     return(
@@ -27,6 +28,21 @@ export default function Home(){
                 <section className='secao2'>
                     <div className='secao2-container'>
                         <h1>Trabalhamos com sonhos <br/>e mais que isso, tornamos <br/>eles <span style={{color: '#0BFEFF'}}>realidade</span>_ </h1>
+                        <div className='projetos-container'>
+                            <div className='projeto'>
+                                <img className='imagem-projeto' src={ImagemProjeto} alt=''/> 
+                                <h3>Nome do Projeto</h3>
+                                <p>Descrição que é uma frase inicial sobre o projeto e para quem foi feito</p>
+                            </div>
+                            
+                            <div className='projeto'>
+                                <img className='imagem-projeto' src={ImagemProjeto} alt=''/> 
+                                <h3>Nome do Projeto</h3>
+                                <p>Descrição que é uma frase inicial sobre o projeto e para quem foi feito</p>
+                            </div>
+
+
+                        </div>
                     </div>
                 </section>
 
