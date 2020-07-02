a9bce08ecdb9537f3dd2aa3e47727c3c9c4bc66d
Michelly Lima <michelly_slima@outlook.com>
2020-07-02 17:03:38 -0300

    fix conflicts

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Portfolio from './pages/Portfolio'
+import Projects from './pages/Projects'
 import Servicos from './pages/Servicos'
 import Contato from './pages/Contato'
 import About from './pages/About'
@@ -12,7 +12,7 @@ export default function Routes(){
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
-                <Route path="/portfolio" component={Portfolio} />
+                <Route path="/projetos" component={Projects} />
                 <Route path="/servicos" component={Servicos} />
                 <Route path="/contato" component={Contato} />
                 <Route path="/about" component={About} />
